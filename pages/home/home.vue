dcbcd4d6a50eca502c8242a963aa8a187c8cc116
李文艺 <1369009517@qq.com>
2022-09-08 07:44:41 +0800

    完成了搜索search功能

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,9 @@
 <template>
   <view class="home-container">
+    <!-- 使用自定义的搜索组件 -->
+    <view class="search-box">
+      <my-search @click="gotoSearch"></my-search>
+    </view>
     <!-- 轮播图区域 -->
     <swiper :indicator-dots="true"
     :autoplay="true" :interval="3000" 
@@ -108,9 +112,15 @@
           })
         })
         this.floorList = res.message
-      }
+      },
+      // 跳转到分包中的搜索页面
+         gotoSearch() {
+           uni.navigateTo({
+             url: '/subpkg/search/search'
+           })
+         }
     },
-    // 声明周期钩子：页面刚加载时触发
+    // 生命周期钩子：页面刚加载时触发
     onLoad() {
       // 调用获取轮播图列表
       this.getSwiperList()
@@ -123,6 +133,15 @@
 </script>
 
 <style lang="scss">
+  // 自定义的搜索组件
+  .search-box {
+    // 设置定位效果为“吸顶”
+    position: sticky;
+    // 吸顶的“位置”
+    top: 0;
+    // 提高层级，防止被轮播图覆盖
+    z-index: 999;
+  }
   // 轮播图
   swiper{
     height: 330rpx;
